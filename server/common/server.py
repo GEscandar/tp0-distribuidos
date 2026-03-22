66dc3f4fa27bc40582da10996455bafdb3caa97a
German Escandar <gescandar@fi.uba.ar>
2026-03-22 05:03:40 +0000

    agregar manejo de conexiones y seniales a server

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,36 @@
 import socket
 import logging
+import signal
+from threading import Thread
 
+class Connection(Thread):
+    def __init__(self, sock: socket.socket):
+        super().__init__()
+        self.sock = sock
+        self.closed = False
+        self.start()
+        
+    def run(self):
+        while not self.closed:
+            try:
+                # TODO: Modify the receive to avoid short-reads
+                msg = self.sock.recv(1024).rstrip().decode('utf-8')
+                if not msg:
+                   logging.info("Client disconnected gracefully")
+                   self.close()
+                   break
+                addr = self.sock.getpeername()
+                logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
+                # TODO: Modify the send to avoid short-writes
+                self.sock.send("{}\n".format(msg).encode('utf-8'))
+            except ConnectionResetError:
+                logging.error(f"Connection reset by client {addr[0]}")
+                self.close()
+        
+    def close(self):
+        if not self.closed:
+            self.sock.close()
+            self.closed = True
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,6 +38,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._connections = []
 
     def run(self):
         """
@@ -17,31 +48,11 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
-
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
+        signal.signal(signal.SIGTERM, self.sig_handler)
+        signal.signal(signal.SIGINT, self.sig_handler)
         while True:
             client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
-
-    def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
-        try:
-            # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
-            # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
-        except OSError as e:
-            logging.error("action: receive_message | result: fail | error: {e}")
-        finally:
-            client_sock.close()
+            self._connections.append(Connection(client_sock))
 
     def __accept_new_connection(self):
         """
@@ -56,3 +67,17 @@ class Server:
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
+    
+    def sig_handler(self, signum, frame):
+        if signum == signal.SIGINT:
+            logging.info("Keyboard Interrupt detected, exiting...")
+        else:
+            logging.info("Server forcefully terminated, exiting...")
+        self.exit()
+    
+    def exit(self):
+        for conn in self._connections:
+            conn.close()
+            conn.join()
+        self._server_socket.close()
+        exit(0)
