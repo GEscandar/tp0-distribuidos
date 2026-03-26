3ce1bb4fcc70f28da7bf81c79684d237881349d7
German Escandar <gescandar@fi.uba.ar>
2026-03-26 09:39:41 -0300

    fix

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 msg="Test_Echo_Message_123"
-network="tp0_testing_net"
+network="tp0-base_testing_net"
 
 resp=$(docker run --rm --network "$network" busybox sh -c "echo '$msg' | nc server 12345")
 
-if [ "$resp" == "$msg" ]; then
+if [[ "$resp" == "$msg" ]]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
