fe57050651b5ab567e8312030f3d6656c7752f66
German Escandar <gescandar@fi.uba.ar>
2026-03-26 02:22:25 -0300

    agregar script de validacion

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+msg="Test_Echo_Message_123"
+network="tp0_testing_net"
+
+resp=$(docker run --rm --network "$network" busybox sh -c "echo '$msg' | nc server 12345")
+
+if [ "$resp" == "$msg" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
